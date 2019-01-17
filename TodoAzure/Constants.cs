eb3e0f65c924c25e65ab28b78ac02798144542f6
David J. Garcia <davidj.garcastillo@gmail.com>
2019-01-17 10:18:46 -0600

    Update Constants.cs

--- TodoAzure/Constants.cs
+++ TodoAzure/Constants.cs
@@ -3,6 +3,6 @@
     public static class Constants
     {
         // Replace string with your mobile service URL.
-        public static string ApplicationURL = @"https://demotodoappdjgc.azurewebsites.net";
+        public static string ApplicationURL = @"<APP_SERVICE_AQUI>";
     }
 }
